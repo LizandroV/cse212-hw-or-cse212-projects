6843d3f4993f3edbfb5a6d856f0ee6242b08a1a0
LizandroV <61571830+LizandroV@users.noreply.github.com>
2025-08-10 20:28:07 -0500

    Week 06 code solution

---
+++ data.js
@@ -0,0 +1,26 @@
+// ARRAYS
+
+let array = [10, 20, 30];
+array.push(40);
+console.log(array);
+let last = array[3];
+array.pop();
+console.log(last);
+console.log(array);
+
+console.log(array.includes(20));
+
+let set = false;
+
+for (i = 0; i < array.length; i++) {
+  if (array[i] == 20) {
+    set = true;
+  }
+}
+
+console.log(set);
+console.log(array.length);
+
+// LINKED LIST
+let myList = myList;
+console.log(myList);
--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,33 +13,56 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data)
+            return; // Stop if value already exists in the tree
+
         if (value < Data)
         {
-            // Insert to the left
+            // Go left if the value is smaller
             if (Left is null)
                 Left = new Node(value);
             else
-                Left.Insert(value);
+                Left.Insert(value); // Keep going left
         }
         else
         {
-            // Insert to the right
+            // Go right if the value is bigger
             if (Right is null)
                 Right = new Node(value);
             else
-                Right.Insert(value);
+                Right.Insert(value); // Keep going right
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true; // Found the value
+
+        if (value < Data)
+        {
+            // Search left side if smaller
+            if (Left is null)
+                return false; // Not found
+            return Left.Contains(value);
+        }
+        else
+        {
+            // Search right side if bigger
+            if (Right is null)
+                return false; // Not found
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = (Left != null) ? Left.GetHeight() : 0;
+        int rightHeight = (Right != null) ? Right.GetHeight() : 0;
+
+        // Height is 1 (this node) + the tallest subtree
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
